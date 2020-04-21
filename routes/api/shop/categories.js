df3796ad4127a9e685019fb506a91561f18ebbc1
ZRichV <597497946@qq.com>
2020-04-21 18:07:22 +0800

    update-routerCate-attrService

--- routes/api/shop/categories.js
+++ routes/api/shop/categories.js
@@ -6,11 +6,11 @@ var catServ = authorization.getService("CategoryService");
 var attrServ = authorization.getService("AttributeService");
 
 // 商品分类数据列表
-router.get('/', function (req, res, next) {
-    if (!req.query.pagenum || req.query.pagenum <= 0) return res.sendResult(null, 400, "pagenum 参数错误");
-    if (!req.query.pagesize || req.query.pagesize <= 0) return res.sendResult(null, 400, "pagesize 参数错误");
+router.get('/', function(req, res, next) {
+    //if (!req.query.pagenum || req.query.pagenum <= 0) return res.sendResult(null, 400, "pagenum 参数错误");
+    //if (!req.query.pagesize || req.query.pagesize <= 0) return res.sendResult(null, 400, "pagesize 参数错误");
     next();
-}, function (req, res, next) {
+}, function(req, res, next) {
     var conditions = null;
     if (req.query.pagenum && req.query.pagesize) {
         conditions = {
@@ -18,80 +18,80 @@ router.get('/', function (req, res, next) {
             "pagesize": req.query.pagesize
         };
     }
-    catServ.getAllCat(req.query.type, conditions, function (err, result) {
+    catServ.getAllCat(req.query.type, conditions, function(err, result) {
         if (err) return res.sendResult(null, 400, '获取商品分类数据列表失败');
         res.sendResult(result, 200, '获取成功');
     })(req, res, next);
 });
 
 // 添加分类
-router.post('/', function (req, res, next) {
+router.post('/', function(req, res, next) {
     if (!req.body.cat_name) return res.sendResult(nul, 400, '分类名称不能为空');
     next();
-}, function (req, res, next) {
+}, function(req, res, next) {
     catServ.addCategory({
         "cat_pid": req.body.cat_pid,
         "cat_name": req.body.cat_name,
         "cat_level": req.body.cat_level
-    }, function (err, result) {
+    }, function(err, result) {
         if (err) return res.sendResult(null, 400, err);
         res.sendResult(result, 201, "添加分类成功");
     })(req, res, next);
 });
 
 // 根据 id 查询分类
-router.get("/:id", function (req, res, next) {
+router.get("/:id", function(req, res, next) {
     if (!req.params.id) return res.sendResult(null, 400, "分类ID不能为空");
     if (isNaN(parseInt(req.params.id))) return res.sendResult(null, 400, "分类ID必须是数字");
     next();
-}, function (req, res, next) {
-    catServ.getCatById(req.params.id, function (err, result) {
+}, function(req, res, next) {
+    catServ.getCatById(req.params.id, function(err, result) {
         if (err) return res.sendResult(null, 400, err);
         res.sendResult(result, 200, "根据 id 查询分类成功");
     })(req, res, next);
 });
 
 // 编辑提交分类
-router.put("/:id", function (req, res, next) {
+router.put("/:id", function(req, res, next) {
     if (!req.params.id) return res.sendResult(null, 400, "分类ID不能为空");
     if (isNaN(parseInt(req.params.id))) return res.sendResult(null, 400, "分类ID必须是数字");
     if (!req.body.cat_name || req.body.cat_name == "") return res.sendResult(null, 400, "分类名称不能为空");
     next();
-}, function (req, res, next) {
-    catServ.updateCat(req.params.id, req.body.cat_name, function (err, result) {
+}, function(req, res, next) {
+    catServ.updateCat(req.params.id, req.body.cat_name, function(err, result) {
         if (err) return res.sendResult(null, 400, err);
         res.sendResult(result, 200, "编辑提交分类成功");
     })(req, res, next);
 });
 
 // 删除分类
-router.delete("/:id", function (req, res, next) {
+router.delete("/:id", function(req, res, next) {
     if (!req.params.id) return res.sendResult(null, 400, "分类ID不能为空");
     if (isNaN(parseInt(req.params.id))) return res.sendResult(null, 400, "分类ID必须是数字");
     next();
-}, function (req, res, next) {
-    catServ.deleteCat(req.params.id, function (msg) {
+}, function(req, res, next) {
+    catServ.deleteCat(req.params.id, function(msg) {
         res.sendResult(null, 200, msg);
     })(req, res, next);
 });
 
 // 参数列表:通过参数方式查询静态参数还是动态参数
-router.get("/:id/attributes", function (req, res, next) {
+router.get("/:id/attributes", function(req, res, next) {
     if (!req.params.id) return res.sendResult(null, 400, "分类ID不能为空");
     if (isNaN(parseInt(req.params.id))) return res.sendResult(null, 400, "分类ID必须是数字");
     if (!req.query.sel || (req.query.sel != "only" && req.query.sel != "many")) {
         return res.sendResult(null, 400, "属性类型必须设置");
     }
     next();
-}, function (req, res, next) {
-    attrServ.getAttributes(req.params.id, req.query.sel, function (err, attributes) {
+}, function(req, res, next) {
+    attrServ.getAttributes(req.params.id, req.query.sel, function(err, attributes) {
         if (err) return res.sendResult(null, 400, err);
         res.sendResult(attributes, 200, "获取成功");
     })(req, res, next);
 });
 
 // 添加动态参数或者静态属性
-router.post("/:id/attributes", function (req, res, next) {
+router.post("/:id/attributes", function(req, res, next) {
     if (!req.params.id) return res.sendResult(null, 400, "分类ID不能为空");
     if (isNaN(parseInt(req.params.id))) return res.sendResult(null, 400, "分类ID必须是数字");
     if (!req.body.attr_name) return res.sendResult(null, 400, "参数名称不能为空");
@@ -103,49 +103,49 @@ router.post("/:id/attributes", function (req, res, next) {
         return res.sendResult(null,400,"参数的 attr_write 必须为 manual 或 list");
     }*/
     next();
-}, function (req, res, next) {
+}, function(req, res, next) {
     attrServ.createAttribute({
         "attr_name": req.body.attr_name,
         "cat_id": req.params.id,
         "attr_sel": req.body.attr_sel,
-        "attr_write": req.body.attr_sel == "many" ? "list" : "manual",//req.body.attr_write,
+        "attr_write": req.body.attr_sel == "many" ? "list" : "manual", //req.body.attr_write,
         "attr_vals": req.body.attr_vals ? req.body.attr_vals : ""
-    }, function (err, attr) {
+    }, function(err, attr) {
         if (err) return res.sendResult(null, 400, err);
         res.sendResult(attr, 201, "添加动态参数或者静态属性成功");
     })(req, res, next);
 });
 
 // 删除参数
-router.delete("/:id/attributes/:attrId", function (req, res, next) {
+router.delete("/:id/attributes/:attrId", function(req, res, next) {
     if (!req.params.id) return res.sendResult(null, 400, "分类ID不能为空");
     if (isNaN(parseInt(req.params.id))) return res.sendResult(null, 400, "分类ID必须是数字");
     if (!req.params.attrId) return res.sendResult(null, 400, "参数ID不能为空");
     if (isNaN(parseInt(req.params.attrId))) return res.sendResult(null, 400, "参数ID必须是数字");
     next();
-}, function (req, res, next) {
-    attrServ.deleteAttribute(req.params.attrId, function (err, newAttr) {
+}, function(req, res, next) {
+    attrServ.deleteAttribute(req.params.attrId, function(err, newAttr) {
         if (err) return res.sendResult(null, 400, err);
         res.sendResult(newAttr, 200, "删除参数成功");
     })(req, res, next);
 });
 
 // 根据 ID 查询参数
-router.get("/:id/attributes/:attrId", function (req, res, next) {
+router.get("/:id/attributes/:attrId", function(req, res, next) {
     if (!req.params.id) return res.sendResult(null, 400, "分类ID不能为空");
     if (isNaN(parseInt(req.params.id))) return res.sendResult(null, 400, "分类ID必须是数字");
     if (!req.params.attrId) return res.sendResult(null, 400, "参数ID不能为空");
     if (isNaN(parseInt(req.params.attrId))) return res.sendResult(null, 400, "参数ID必须是数字");
     next();
-}, function (req, res, next) {
-    attrServ.attributeById(req.params.attrId, function (err, attr) {
+}, function(req, res, next) {
+    attrServ.attributeById(req.params.attrId, function(err, attr) {
         if (err) return res.sendResult(null, 400, err);
         res.sendResult(attr, 200, "根据 ID 查询参数成功");
     })(req, res, next);
 });
 
 // 编辑提交参数
-router.put("/:id/attributes/:attrId", function (req, res, next) {
+router.put("/:id/attributes/:attrId", function(req, res, next) {
     if (!req.params.id) return res.sendResult(null, 400, "分类ID不能为空");
     if (isNaN(parseInt(req.params.id))) return res.sendResult(null, 400, "分类ID必须是数字");
     if (!req.params.attrId) return res.sendResult(null, 400, "参数ID不能为空");
@@ -155,18 +155,19 @@ router.put("/:id/attributes/:attrId", function (req, res, next) {
     }
     if (!req.body.attr_name || req.body.attr_name == "") return res.sendResult(null, 400, "参数名称不能为空");
     next();
-}, function (req, res, next) {
+}, function(req, res, next) {
     attrServ.updateAttribute(
         req.params.attrId, {
-        "attr_name": req.body.attr_name,
-        "cat_id": req.params.id,
-        "attr_sel": req.body.attr_sel,
-        "attr_write": req.body.attr_sel == "many" ? "list" : "manual",//req.body.attr_write,
-        "attr_vals": req.body.attr_vals ? req.body.attr_vals : ""
-    }, function (err, newAttr) {
-        if (err) return res.sendResult(null, 400, err);
-        res.sendResult(newAttr, 200, "编辑提交参数成功");
-    })(req, res, next);
+            "attr_name": req.body.attr_name,
+            "cat_id": req.params.id,
+            "attr_sel": req.body.attr_sel,
+            "attr_write": req.body.attr_sel == "many" ? "list" : "manual", //req.body.attr_write,
+            "attr_vals": req.body.attr_vals ? req.body.attr_vals : ""
+        },
+        function(err, newAttr) {
+            if (err) return res.sendResult(null, 400, err);
+            res.sendResult(newAttr, 200, "编辑提交参数成功");
+        })(req, res, next);
 });
 
 // 
--- services/AttributeService.js
+++ services/AttributeService.js
@@ -1,7 +1,7 @@
 var _ = require('lodash');
 var path = require("path");
-var dao = require(path.join(process.cwd(),"dao/DAO"));
-var attributeDao = require(path.join(process.cwd(),"dao/AttributeDAO"));
+var dao = require(path.join(process.cwd(), "dao/DAO"));
+var attributeDao = require(path.join(process.cwd(), "dao/AttributeDAO"));
 
 /**
  * 获取属性列表
@@ -9,9 +9,9 @@ var attributeDao = require(path.join(process.cwd(),"dao/AttributeDAO"));
  * @param {*} sel 
  * @param {*} cb 
  */
-module.exports.getAttributes = function(cat_id, sel, cb){
-    attributeDao.list(cat_id, sel, function(err, attributes){
-        if(err) return cb('获取属性列表失败');
+module.exports.getAttributes = function(cat_id, sel, cb) {
+    attributeDao.list(cat_id, sel, function(err, attributes) {
+        if (err) return cb('获取属性列表失败');
         cb(null, attributes);
     });
 }
@@ -21,10 +21,10 @@ module.exports.getAttributes = function(cat_id, sel, cb){
  * @param {*} info 
  * @param {*} cb 
  */
-module.exports.createAttribute = function(info, cb){
-    dao.create("AttributeModel", info, function(err,attribute){
-        if(err) return cb("创建参数失败");
-        cb(null, attributes);
+module.exports.createAttribute = function(info, cb) {
+    dao.create("AttributeModel", info, function(err, attribute) {
+        if (err) return cb("创建参数失败");
+        cb(null, attribute);
     });
 }
 
@@ -34,9 +34,9 @@ module.exports.createAttribute = function(info, cb){
  * @param {*} info 
  * @param {*} cb 
  */
-module.exports.updateAttribute = function(attrId, info, cb){
-    dao.update('AttributeModel', attrId, info, function(err, newAttr){
-        if(err) return cb(err);
+module.exports.updateAttribute = function(attrId, info, cb) {
+    dao.update('AttributeModel', attrId, info, function(err, newAttr) {
+        if (err) return cb(err);
         cb(null, _.omit(newAttr, "deleted_time"));
     });
 }
@@ -46,11 +46,11 @@ module.exports.updateAttribute = function(attrId, info, cb){
  * @param {*} attrId 
  * @param {*} cb 
  */
-module.exports.deleteAttribute = function(attrId, cb){
+module.exports.deleteAttribute = function(attrId, cb) {
     dao.update('AttributeModel', attrId, {
-        "delete_time": parseInt((Date.now()/1000))
-    }, function(err, newAttr){
-        if(err) return cb('删除参数失败');
+        "delete_time": parseInt((Date.now() / 1000))
+    }, function(err, newAttr) {
+        if (err) return cb('删除参数失败');
         cb(null, newAttr);
     });
 }
@@ -60,9 +60,9 @@ module.exports.deleteAttribute = function(attrId, cb){
  * @param {*} attrId 
  * @param {*} cb 
  */
-module.exports.attributeById = function(attrId, cb){
-    dao.show('AttributeModel', attrId, function(err, attr){
-        if(err) return cb(err);
+module.exports.attributeById = function(attrId, cb) {
+    dao.show('AttributeModel', attrId, function(err, attr) {
+        if (err) return cb(err);
         cb(null, _.omit(attr, "delete_time"));
     });
 }
